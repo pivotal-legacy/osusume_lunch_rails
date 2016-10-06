6a6290105f75ee26e63581da973a42dec84afab8
Walter Scarborough <wscarborough@pivotal.io>
2016-10-06 14:09:52 +0900

    Add test for restaurant#index
    Signed-off-by: Chiharu Miya <cmiya@pivotal.io>
    Signed-off-by: Ichizo Umehara <iumehara@pivotal.io>

--- spec/requests/restaurants_spec.rb
+++ spec/requests/restaurants_spec.rb
@@ -2,9 +2,20 @@ require 'rails_helper'
 
 RSpec.describe 'Restaurants', type: :request do
   describe 'GET /restaurants' do
-    it 'works! (now write some real specs)' do
+    it 'returns a list of restaurants' do
+      restaurants = [
+        Restaurant.create(name: 'Afuri'),
+        Restaurant.create(name: 'Butagumi')
+      ]
+
       get restaurants_path
+
+      response_restaurants = JSON.parse(response.body)
+      restaurant_names = restaurants.map(&:name)
+
       expect(response).to have_http_status(200)
+      expect(response_restaurants.count).to equal(2)
+      expect(restaurant_names).to include(response_restaurants[0]['name'])
     end
   end
 
