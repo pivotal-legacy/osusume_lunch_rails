0801b9414a7d7585c1defa3fbdbd508640db2189
Chiharu Miya <cmiya@pivotal.io>
2016-10-14 16:40:15 +0900

    Alphabetize restaurant list order
    Signed-off-by: Ichizo Umehara <iumehara@pivotal.io>

--- spec/requests/restaurants_spec.rb
+++ spec/requests/restaurants_spec.rb
@@ -4,18 +4,17 @@ RSpec.describe 'Restaurants', type: :request do
   describe 'GET /restaurants' do
     it 'returns a list of restaurants' do
       restaurants = [
-        Restaurant.create(name: 'Afuri'),
-        Restaurant.create(name: 'Butagumi')
+        Restaurant.create(name: 'Butagumi'),
+        Restaurant.create(name: 'Afuri')
       ]
 
       get restaurants_path
 
       response_restaurants = JSON.parse(response.body)['restaurants']
-      restaurant_names = restaurants.map(&:name)
 
       expect(response).to have_http_status(200)
       expect(response_restaurants.count).to equal(2)
-      expect(restaurant_names).to include(response_restaurants[0]['name'])
+      expect(response_restaurants[0]['name']).to eq('Afuri')
     end
   end
 
